d15e5d6526c6a7e2e0518d0be4aac8ae4c45996b
kadenweaver <kadenpweaver@gmail.com>
2023-03-08 19:23:16 -0700

    make dark mode the default

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import AppUI from './App';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react'
 import '@fontsource/eb-garamond/700.css'
 import '@fontsource/noto-sans-jp/400.css'
 
 const theme = extendTheme({
   heading: `'Noto Sans JP', sans-serif`,
-  text: `'EB Garamond', serif`
+  text: `'EB Garamond', serif`,
+  config: {initialColorMode: 'dark', useSystemColorMode: false},
 })
 
 const container = document.getElementById('root')
@@ -18,6 +19,7 @@ const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <AppUI />
     </ChakraProvider>
   </React.StrictMode>
